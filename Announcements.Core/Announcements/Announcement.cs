74aaddee51fd33c25804218a12d3ba8232e11a5c
Mahsun Dağ <mahsun.dag@atolla.com.tr>
2023-12-31 16:36:41 +0300

    Core layer is done

---
+++ Announcements.Core/Announcements/Announcement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Announcements.Core.Announcements
+{
+    public sealed class Announcement:BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string ShortText { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public AnnouncementDetail AnnouncementDetail { get; set; }
+    }
+}
---
+++ Announcements.Core/Announcements/AnnouncementDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Announcements.Core.Announcements
+{
+    public sealed class AnnouncementDetail
+    {
+        public int Id { get; set; }
+        public string Detail { get; set; }
+
+        public int AnnouncementId { get; set; }
+
+        public Announcement Announcement { get; set; }
+    }
+}
