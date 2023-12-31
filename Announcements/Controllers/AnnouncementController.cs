7269a76520db19fd7369895b648593bbe387ebd2
Mahsun Dağ <mahsun.dag@atolla.com.tr>
2024-01-01 00:59:31 +0300

    SwaggerOperations Added

--- Announcements/Controllers/AnnouncementController.cs
+++ Announcements/Controllers/AnnouncementController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Announcements.Controllers
 {
@@ -20,8 +21,14 @@ namespace Announcements.Controllers
             _mapper = mapper;
             _service = service;
         }
-
+        /// <summary>
+        /// Gets page number and page size and return elements in that range
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns> list of specified range elements  </returns>
         [HttpGet]
+        [SwaggerOperation(Summary = "Get Announcements By pager number and page size", Description = "Get Announcements By pager number and page size")]
         public async Task<IActionResult> All(int pageNumber = 1,int pageSize = 10)
         {
             var annoumcements = await _service.GetAllAsync();
@@ -34,7 +41,13 @@ namespace Announcements.Controllers
             return Ok(annDtos);
 
         }
+        /// <summary>
+        /// Gets slug of the Announcement and returns that spesific item
+        /// </summary>
+        /// <param name="slug"></param>
+        /// <returns>spesific item which is called by slug endpoint</returns>
         [HttpGet("{slug}")]
+        [SwaggerOperation(Summary = "Get Announcement By Slug", Description = "Get Announcement By Slug")]
         public async Task<ActionResult<Announcement>> GetAnnouncementBySlug(string slug)
         {
             var announcements = _service.Where(a => a.Slug == slug);
--- Announcements/Program.cs
+++ Announcements/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
