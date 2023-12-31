033c9687d12243c841eeea0ef98bb4346cb67e7c
Mahsun Dağ <mahsun.dag@atolla.com.tr>
2023-12-31 22:11:51 +0300

    Dtos are created

---
+++ Announcements.Core/DTOs/BaseEntityDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Announcements.Core.DTOs
+{
+    public abstract class BaseEntityDto:BaseDto
+    {
+        public DateTime  CreatedTime { get; set; }
+
+        public DateTime UpdatedTime { get; set;}
+    }
+}
