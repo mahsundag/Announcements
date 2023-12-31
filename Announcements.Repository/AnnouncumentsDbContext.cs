2b52ae721f257391bff019e96fa2a983b5c8dd23
Mahsun Dağ <mahsun.dag@atolla.com.tr>
2023-12-31 16:51:24 +0300

    Db Context Added

---
+++ Announcements.Repository/AnnouncumentsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Announcements.Core.Announcements;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Announcements.Repository
+{
+    public class AnnouncumentsDbContext:DbContext
+    {
+        public AnnouncumentsDbContext(DbContextOptions<AnnouncumentsDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Announcement> Announcements { get; set; }
+    }
+}
