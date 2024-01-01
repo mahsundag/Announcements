3fb0f117daf666b8db480864f4362c801091a9f5
Mahsun Dağ <mahsun.dag@atolla.com.tr>
2024-01-01 20:01:39 +0300

    Announcement Changes

---
+++ Announcements.Core/DTOs/AnnouncementDTOs/Announcements/AnnouncementResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Announcements.Core.DTOs.AnnouncementDTOs.Announcements
+{
+    public class AnnouncementResponseDto
+    {
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string ShortText { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Detail { get; set; }
+    }
+}
--- Announcements/Controllers/AnnouncementController.cs
+++ Announcements/Controllers/AnnouncementController.cs
@@ -1,6 +1,7 @@
 ﻿using Announcements.Core;
 using Announcements.Core.Announcements;
 using Announcements.Core.DTOs.AnnouncementDTOs.Announcements;
+using Announcements.Service.Services.AnnouncementDetails;
 using Announcements.Service.Services.Announcements;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -16,12 +17,14 @@ namespace Announcements.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IAnnouncementService _service;
+        private readonly IAnnouncementDetailService _detailService;
         private readonly ILogger<AnnouncementController> _logger;
-        public AnnouncementController(IMapper mapper, IAnnouncementService service, ILogger<AnnouncementController> logger)
+        public AnnouncementController(IMapper mapper, IAnnouncementService service, IAnnouncementDetailService detailService, ILogger<AnnouncementController> logger)
         {
             _mapper = mapper;
             _service = service;
             _logger = logger;
+            _detailService = detailService;
         }
         /// <summary>
         /// Gets page number and page size and return elements in that range
@@ -31,12 +34,12 @@ namespace Announcements.Controllers
         /// <returns> list of specified range elements  </returns>
         [HttpGet]
         [SwaggerOperation(Summary = "Get Announcements By pager number and page size", Description = "Get Announcements By pager number and page size")]
-        public async Task<IActionResult> GetAll(int pageNumber = 1,int pageSize = 10)
+        public async Task<IActionResult> GetAll(int pageNumber = 1, int pageSize = 10)
         {
             _logger.LogInformation("Getting All Data");
             var annoumcements = await _service.GetAllAsync();
             var announcementsCount = annoumcements.Count();
-            var pagination =  annoumcements
+            var pagination = annoumcements
                                  .Skip((pageNumber - 1) * pageSize)
                                  .Take(pageSize);
             var result = new AnnouncementListResponseDto
@@ -56,16 +59,32 @@ namespace Announcements.Controllers
         /// <returns>spesific item which is called by slug endpoint</returns>
         [HttpGet("{slug}")]
         [SwaggerOperation(Summary = "Get Announcement By Slug", Description = "Get Announcement By Slug")]
-        public Task<ActionResult<Announcement>> GetAnnouncementBySlug(string slug)
+        public Task<ActionResult<AnnouncementResponseDto>> GetAnnouncementBySlug(string slug)
         {
             var announcements = _service.Where(a => a.Slug == slug);
 
             if (!announcements.Any())
             {
                 _logger.LogInformation("No Data.");
-                return Task.FromResult<ActionResult<Announcement>>(NotFound());
+                return Task.FromResult<ActionResult<AnnouncementResponseDto>>(NotFound());
             }
-            return Task.FromResult<ActionResult<Announcement>>(Ok(announcements.FirstOrDefault()));
+            var res = new AnnouncementResponseDto { };
+            var announcement = announcements.FirstOrDefault();
+            if (announcement is not null)
+            {
+                res.Slug = announcement.Slug;
+                res.Title = announcement.Title;
+                res.ShortText = announcement.ShortText;
+                res.Date = announcement
+                    .Date;
+                var detail = _detailService.Where(x => x.AnnouncementId == announcement.Id);
+                if (detail is not null)
+                {
+                   var det = detail.First();
+                   res.Detail = det.Detail;
+                }
+            }
+            return Task.FromResult<ActionResult<AnnouncementResponseDto>>(Ok(res));
         }
         /// <summary>
         /// Gets Announcement with Id , we added id:int for the multiple root match exception
